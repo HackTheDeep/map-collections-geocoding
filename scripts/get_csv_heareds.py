36a0e1861d6778288bcd7efdedd540b386f8b3cb
Pedro Baumann <ondoheer@gmail.com>
2018-02-10 16:15:41 -0500

    renamed csvs

---
+++ scripts/get_csv_heareds.py
@@ -0,0 +1,16 @@
+import csv 
+
+FILES = [
+	'herpetology_seasnakes',
+	'ichthyology_marine_collections',
+	'mammalogy_marine_mammals',
+	'ornithology_specimens_aquatic_localities',
+
+]
+
+
+for file in FILES:
+	headers = [];
+
+
+

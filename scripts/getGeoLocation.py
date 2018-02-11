170c9c13390449ca3f4da6c8a157e0f9a2e3c20f
Tyler Cummings-Bond <tyler.cummings.bond@gmail.com>
2018-02-11 06:10:03 -0500

    clean up, allow a choice of preferred API

--- scripts/getGeoLocation.py
+++ scripts/getGeoLocation.py
@@ -1,27 +1,25 @@
-import pip
+import geocoder
+import googlemaps
 
-try:
-	import geocoder
-except ImportError:
-	pip.main(['install','geocoder'])
-	import geocoder
-	
-try:
-	import googlemaps
-except ImportError:
-	pip.main(['install','googlemaps'])
-	import googlemaps
+def getLatLon(locationString, geoCodeKey='***REMOVED***', googleKey='***REMOVED***',preferred='geocoder'):
+	ret_val = getLocationGeocoder(locationString, geoCodeKey) if preferred=='geocoder' else getLocationGoogle(locationString, googleKey)
+	if(ret_val["found"]==False):
+		ret_val = getLocationGoogle(locationString, googleKey) if preferred=='geocoder' else getLocationGeocoder(locationString, geoCodeKey)
+	return ret_val
 
-def getLatLon(locationString, geoCodeKey='***REMOVED***', googleKey='***REMOVED***'):
-	g = geocoder.geonames(locationString, key=geoCodeKey)
+def getLocationGeocoder(locationString, a_key):
+	g = geocoder.geonames(locationString, key=a_key)
 	if(g.lat is None):
-		gmaps = googlemaps.Client(key=googleKey)
-		result = gmaps.geocode(locationString)
-		if(len(result)>0):
-			retVal = result[0]['geometry']['location']
-			retVal.update({"found":True})
-			return retVal
-		else:
-			return {"found":False}
+		return {"found":False}
 	else:
 		return {'lat':g.lat,'lng':g.lng,"found":True}
+
+def getLocationGoogle(locationString, a_key):
+	gmaps = googlemaps.Client(key=a_key)
+	result = gmaps.geocode(locationString)
+	if(len(result)>0):
+		retVal = result[0]['geometry']['location']
+		retVal.update({"found":True})
+		return retVal
+	else:
+		return {"found":False}

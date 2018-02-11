f8d29673acdada19b8399bcb14d2828b80b61c0e
Tyler Cummings-Bond <tyler.cummings.bond@gmail.com>
2018-02-11 07:12:14 -0500

    add Bing as a service, allow control by list

--- scripts/getGeoLocation.py
+++ scripts/getGeoLocation.py
@@ -1,21 +1,17 @@
 import geocoder
 import googlemaps
+from bingmaps.apiservices import LocationByQuery
+from keys import googleKey, geoCodeKey, bingKey
 
-def getLatLon(locationString, geoCodeKey='***REMOVED***', googleKey='***REMOVED***',preferred='geocoder'):
-	ret_val = getLocationGeocoder(locationString, geoCodeKey) if preferred=='geocoder' else getLocationGoogle(locationString, googleKey)
-	if(ret_val["found"]==False):
-		ret_val = getLocationGoogle(locationString, googleKey) if preferred=='geocoder' else getLocationGeocoder(locationString, geoCodeKey)
-	return ret_val
-
-def getLocationGeocoder(locationString, a_key):
-	g = geocoder.geonames(locationString, key=a_key)
+def getLocationGeocoder(locationString):
+	g = geocoder.geonames(locationString, key=geoCodeKey)
 	if(g.lat is None):
 		return {"found":False}
 	else:
 		return {'lat':g.lat,'lng':g.lng,"found":True}
 
-def getLocationGoogle(locationString, a_key):
-	gmaps = googlemaps.Client(key=a_key)
+def getLocationGoogle(locationString):
+	gmaps = googlemaps.Client(key=googleKey)
 	result = gmaps.geocode(locationString)
 	if(len(result)>0):
 		retVal = result[0]['geometry']['location']
@@ -23,3 +19,18 @@ def getLocationGoogle(locationString, a_key):
 		return retVal
 	else:
 		return {"found":False}
+
+def getLocationBing(locationString):
+	result = LocationByQuery({'q':locationString,"key":bingKey,"maxResults":"1"})
+	coords = result.get_coordinates
+	return {"found":False} if len(coords)==0 else {'lat':coords[0].latitude,'lng':coords[0].longitude,"found":True}
+
+geoCoders = {'geocoder':getLocationGeocoder,'google':getLocationGoogle,'bing':getLocationBing}
+
+def getLatLon(locationString,order=['geocoder','google','bing']):
+	param={"locationString":locationString}
+	for function_shorthand in order:
+		ret_val=geoCoders[function_shorthand](**param)
+		if(ret_val["found"]==True):
+			return ret_val
+	return {"found":False}
